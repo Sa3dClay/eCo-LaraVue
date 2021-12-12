f29bd0a044c31228f21d964059e7931403c6b3d3
sa3d clay <sa3d.clay@gmail.com>
2021-12-12 21:47:00 +0200

    seeder name

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class RolesTableSeeder extends Seeder
+class RolesSeeder extends Seeder
 {
     /**
      * Run the database seeds.

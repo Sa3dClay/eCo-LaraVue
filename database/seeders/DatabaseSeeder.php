c33193326191b903885776ca6b8fc58de34c5434
AbdulRahman <30749261+Sa3dClay@users.noreply.github.com>
2022-01-04 22:00:37 +0200

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(DeleteUsersSeeder::class);
         $this->call(AdminSeeder::class);
         
         $this->call(RolesSeeder::class);
